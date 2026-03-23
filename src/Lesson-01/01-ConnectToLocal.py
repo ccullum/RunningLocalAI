5acee8e9e6f0ad6db80efdca60b835c27610ef3e
CCullum <ccullum@yemtech.com>
2026-03-23 08:33:02 -0700

    Cleaned up an extraneous import

--- src/Lesson-01/01-ConnectToLocal.py
+++ src/Lesson-01/01-ConnectToLocal.py
@@ -1,5 +1,6 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv 
  
 # Load environment variables 
